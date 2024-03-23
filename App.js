32e100b808c230d3f0e89917c5263a40c11b5737
BianGode <bian2205@gmail.com>
2024-03-23 14:46:38 +0100

    added mongo db very basic test connection after much struggle

--- App.js
+++ App.js
@@ -1,20 +1,70 @@
-
 // Filename - demoapp/app.js
- 
-const express = require("express");
+import { MongoClient } from "mongodb";
+
+import express from "express";
 const app = express();
- 
+
 app.post("/post", (req, res) => {
-    console.log("Connected to React");
-    res.redirect("/");
+  console.log("Connected to React");
+  res.redirect("/");
 });
- 
-app.get('/test', (req, res) => {
-  res.send({message: 'Hello'});
-})
+
+app.get("/test", (req, res) => {
+  res.send({ message: "Hello" });
+});
+
+let db = 0;
+var url = "mongodb://127.0.0.1:27017/";
+// MongoClient.connect('mongodb://127.0.0.1:27017/', function(err, client) {
+//     if(err) { console.error(err) }
+//     db = client.db('testDb') // once connected, assign the connection to the global variable
+// })
+MongoClient.connect(url)
+  .then((database) => {
+    db = database.db("testDb");
+    console.log("succes");
+  })
+  .then(() => {
+    db.collection("testCollection")
+      .find({test: true})
+      .toArray(function (err, docs) {
+        if (err) {
+          console.error(err);
+        }
+        return JSON.stringify(docs)
+      }).then((res) => {
+        console.log(res);
+      })
+  })
+  .catch((err) => {
+    console.log("Failed", err);
+  });
+
+// app.get('/db', function(req,res) {
+//   db.collection('testCollection').find({}).toArray(function(err, docs) {
+//     if(err) { console.error(err) }
+//     console.log(docs);
+//     // res.send(JSON.stringify(docs))
+// })
+// })
+
+// app.get("/db", (req, res) => {
+//   // database connection
+//   var url = "mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.2.1";
+//   MongoClient.connect(url, function (err, db) {
+
+//     if (err) throw err;
+//     var dbo = db.db("testDb");
+//     var myObj = { name: 'Hello', test: true}
+//     dbo
+//       .collection("testCollection").insertOne(myObj, (err, response) => {
+//         if (err) throw err;
+//         console.log('1 doc insterted');
+//         db.close()
+//       })
+//   });
+// });
 
 const PORT = process.env.PORT || 8080;
- 
-app.listen(PORT,
-    console.log(`Server started on port ${PORT}`)
-);
+
+app.listen(PORT, console.log(`Server started on port ${PORT}`));
